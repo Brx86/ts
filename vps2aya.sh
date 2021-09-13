e72a751c95b9e365b31f1f2bf28030c17ca29b09
brx86 <1124852137@qq.com>
2021-09-13 22:29:23 +0800

    update

--- vps2aya.sh
+++ vps2aya.sh
@@ -36,7 +36,7 @@ Include = /etc/pacman.d/mirrorlist
 AYA
 
 cat > /tmp/archfs/live.sh <<- LIVE
-cat > /root/setup.sh <<- SETUP
+cat > /mnt/setup.sh <<- SETUP
 #!/usr/bin/bash
 #Auther: Ayatale
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -59,7 +59,7 @@ useradd aya -m -G wheel
 echo "请设置root密码:"
 passwd
 echo "请设置aya密码:"
-passwd aya
+passwd ayas
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 mkdir /home/aya/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDB/Hfn+8P9nveMHUSjaJxsJZuiZ7qdfqz9P8K2pYRAskBqkq7/Q7iL/he5ALas0B9UR0oYUPVCmRxBSJ1hci+nZtIjvJqQKcygSGCVvD19Velb6EwMLTP1uCfmkb+4WgcrU9OHBP/qqf3pIHhJolN2o+6c5iQFPkOHfMCkg4XiN14JolUQ/0Q62BH8XlWUisLuv/a0LKsI0jKTQ5RikVD02+g7eoE9e2Db7asqzPUnLknT8MpLcXOemV7/1EJM8cKRQCwRTbOpTPBfNTU7llGXSBn/HNIa58plIt4mOedrNROasIXyYtoLNJ/ij6xaoavrMXoWzG+54NYvLceIlm8uzeletJjO5npaAOhl5i7LnbO9epiYR0owEUY96RCmW1AO/5oC3RbsTttY8EWl2eGsJolrtJ9CYqP96MZoOAYUZYvkFvEVpN8HmtJGNOE5UuxbMkiRLzSGvciF00bYrgmaLGZSRk292jn7w+OU2DFYqWF3cCbMlj/yZqE/sqZC40M= 1124852137@qq.com" > /home/aya/.ssh/authorized_keys
@@ -78,7 +78,7 @@ echo '生成fstab...'
 genfstab -U /mnt >> /mnt/etc/fstab
 umount /etc/resolv.conf
 echo '即将进入Archliux...'
-arch-chroot /mnt bash < /root/setup.sh
+arch-chroot /mnt /setup.sh
 LIVE
 
 rm /tmp/archfs/etc/resolv.conf
@@ -86,6 +86,6 @@ echo 'Server = http://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch' > /tmp/archf
 echo 'nameserver 114.114.114.114' > /tmp/archfs/etc/resolv.conf
 echo '即将进入live系统...'
 mount --bind /tmp/archfs /tmp/archfs
-/tmp/archfs/bin/arch-chroot /tmp/archfs/ bash < /tmp/archfs/live.sh
+/tmp/archfs/bin/arch-chroot /tmp/archfs/ /live.sh
 echo '安装完成，3秒后重启...'
 reboot
